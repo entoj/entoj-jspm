95c1e13b72ce4818441b7c73e26dc19d966874ce
Christian Auth <c.auth@syzygy.de>
2017-06-21 17:34:32 +0200

    feat: makes entoj-js available in the public api

--- source/index.js
+++ source/index.js
@@ -1,7 +1,10 @@
 /**
- * @namespace image
+ * Public Api
  */
 module.exports =
 {
+    linter: require('entoj-js').linter,
+    model: require('entoj-js').model,
+    parser: require('entoj-js').parser,
     server: require('./server/index.js')
 };
