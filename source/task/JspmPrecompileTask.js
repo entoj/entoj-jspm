e017ca48e30b9027724d505a7498bc34bf91cf0b
Christian Auth <c.auth@syzygy.de>
2017-06-27 18:32:32 +0200

    enhance(task.JspmPrecompileTask): extends from EntitiesTask; adds error handling

--- source/task/JspmPrecompileTask.js
+++ source/task/JspmPrecompileTask.js
@@ -4,13 +4,13 @@
  * Requirements
  * @ignore
  */
-const Task = require('entoj-system').task.Task;
+const EntitiesTask = require('entoj-system').task.EntitiesTask;
 const GlobalRepository = require('entoj-system').model.GlobalRepository;
 const PathesConfiguration = require('entoj-system').model.configuration.PathesConfiguration;
 const ContentType = require('entoj-system').model.ContentType;
 const CliLogger = require('entoj-system').cli.CliLogger;
+const ErrorHandler = require('entoj-system').error.ErrorHandler;
 const assertParameter = require('entoj-system').utils.assert.assertParameter;
-const through2 = require('through2');
 const VinylFile = require('vinyl');
 const co = require('co');
 const babel = require('babel-core');
@@ -19,21 +19,21 @@ const babel = require('babel-core');
 /**
  * @memberOf task
  */
-class JspmPrecompileTask extends Task
+class JspmPrecompileTask extends EntitiesTask
 {
     /**
-     *
+     * @param {cli.CliLogger} cliLogger
+     * @param {model.configuration.PathesConfiguration} pathesConfiguration
+     * @param {model.GlobalRepository} globalRepository
      */
     constructor(cliLogger, pathesConfiguration, globalRepository)
     {
-        super(cliLogger);
+        super(cliLogger, globalRepository);
 
         //Check params
         assertParameter(this, 'pathesConfiguration', pathesConfiguration, true, PathesConfiguration);
-        assertParameter(this, 'globalRepository', globalRepository, true, GlobalRepository);
 
         // Assign options
-        this._globalRepository = globalRepository;
         this._pathesConfiguration = pathesConfiguration;
     }
 
@@ -57,36 +57,20 @@ class JspmPrecompileTask extends Task
 
 
     /**
+     * @inheritDocs
+     */
+    get sectionName()
+    {
+        return 'Precompiling js files';
+    }
+
+
+    /**
      * @type {model.configuration.PathesConfiguration}
      */
     get pathesConfiguration()
     {
         return this._pathesConfiguration;
-    }
-
-
-    /**
-     * @type {model.GlobalRepository}
-     */
-    get globalRepository()
-    {
-        return this._globalRepository;
-    }
-
-
-    /**
-     * @protected
-     * @returns {Promise<Array>}
-     */
-    prepareParameters(buildConfiguration, parameters)
-    {
-        const promise = super.prepareParameters(buildConfiguration, parameters)
-            .then((params) =>
-            {
-                params.query = params.query || '*';
-                return params;
-            });
-        return promise;
     }
 
 
@@ -129,71 +113,8 @@ class JspmPrecompileTask extends Task
                 result.push(resultFile);
             }
             return result;
-        });
+        }).catch(ErrorHandler.handler(scope));
         return promise;
-    }
-
-
-    /**
-     * @inheritDocs
-     * @returns {Promise<Array>}
-     */
-    processEntities(buildConfiguration, parameters)
-    {
-        const scope = this;
-        const promise = co(function *()
-        {
-            // Prepare
-            const params = yield scope.prepareParameters(buildConfiguration, parameters);
-
-            // Process each entity
-            const result = [];
-            const entities = yield scope.globalRepository.resolveEntities(params.query);
-            for (const entity of entities)
-            {
-                // Render entity
-                const files = yield scope.processEntity(entity, buildConfiguration, parameters);
-                result.push(...files);
-            }
-
-            // Done
-            return result;
-        });
-        return promise;
-    }
-
-
-    /**
-     * @returns {Stream}
-     */
-    stream(stream, buildConfiguration, parameters)
-    {
-        let resultStream = stream;
-        if (!resultStream)
-        {
-            resultStream = through2(
-                {
-                    objectMode: true
-                });
-            const scope = this;
-            co(function *()
-            {
-                const work = scope._cliLogger.section('Precompiling js files');
-                const params = yield scope.prepareParameters(buildConfiguration, parameters);
-                scope.cliLogger.options(params);
-                const files = yield scope.processEntities(buildConfiguration, parameters);
-                for (const file of files)
-                {
-                    resultStream.write(file);
-                }
-                resultStream.end();
-                scope.cliLogger.end(work);
-            }).catch((e) =>
-            {
-                this.logger.error(e);
-            });
-        }
-        return resultStream;
     }
 }
 
