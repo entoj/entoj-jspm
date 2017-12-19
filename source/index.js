5ecd03330ae8df4844b285743b6e9e430f9a22bf
Christian Auth <c.auth@syzygy.de>
2017-12-19 11:14:11 +0100

    feat: adds simple module registration

--- source/index.js
+++ source/index.js
@@ -1,8 +1,48 @@
 /**
+ * Registers with default configurations
+ */
+function register(configuration, options)
+{
+    // Commands
+    configuration.commands.add(require('./command/index.js').JspmCommand);
+
+    // Linter
+    configuration.commands.add(require('entoj-system').command.LintCommand,
+        {
+            '!linters':
+            [
+                {
+                    type: require('entoj-js').linter.JsFileLinter,
+                    options:
+                    {
+                        useDefaultRules: true
+                    }
+                }
+            ]
+        });
+
+    // Routes
+    configuration.commands.add(require('entoj-system').command.ServerCommand,
+        {
+            options:
+            {
+                routes:
+                [
+                    {
+                        type: require('./server/index.js').route.JspmRoute
+                    }
+                ]
+            }
+        });
+}
+
+
+/**
  * Public Api
  */
 module.exports =
 {
+    register: register,
     command: require('./command/index.js'),
     linter: require('entoj-js').linter,
     model: require('entoj-js').model,
